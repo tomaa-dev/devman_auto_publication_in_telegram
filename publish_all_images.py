f431b974063f0f7d0b5b0a78bbe6935170c83c2f
tomaa-dev <lajpanovatoma@gmail.com>
2025-11-22 23:03:29 +0300

    Avoid connection failures

--- publish_all_images.py
+++ publish_all_images.py
@@ -4,6 +4,7 @@ import random
 import time
 from dotenv import load_dotenv 
 from publish_the_image import publish_for_telegram, take_files
+from telegram.error import NetworkError, TelegramError
 
 
 def publish_all_images(directory, interval, tg_space_token, tg_chat_id):
@@ -11,8 +12,13 @@ def publish_all_images(directory, interval, tg_space_token, tg_chat_id):
         images = take_files(directory)
         random.shuffle(images)
         for image in images:
-            publish_for_telegram(directory, image, tg_space_token, tg_chat_id)
-            time.sleep(interval)
+            try:
+                publish_for_telegram(directory, image, tg_space_token, tg_chat_id)
+                time.sleep(interval)
+
+            except (NetworkError, TelegramError) as e:
+                time.sleep(interval)
+                continue
 
 
 if __name__ == "__main__":
@@ -22,6 +28,6 @@ if __name__ == "__main__":
     tg_space_token = os.environ["TELEGRAM_SPACE_TOKEN"]
     tg_chat_id = os.environ["TELEGRAM_ID"]
 
-    interval = int(os.environ.get("PUBLISH_INTERVAL", 14400))
+    interval = int(os.environ.get("PUBLISH_INTERVAL", 5))
 
     publish_all_images(directory, interval, tg_space_token, tg_chat_id)
